6566c3e0249d48364a00395125709131e2033e73
username <email.com>
2025-01-28 16:45:27 +0200

    info added

--- viewmodel.py
+++ viewmodel.py
@@ -37,25 +37,111 @@ class WeatherViewModel:
                 try:
                     data = self.model.fetch_weather(city_name)
                     if data:
-                        self._update_ui(city_index, f"Temperature: {data['temperature']}°C", f"Condition: {data['weather_condition']}", "")
+                        self._update_ui(
+                            city_index,
+                            f"Temperature: {data['temperature']}°C",
+                            f"Condition: {data['weather_condition']}",
+                            f"{data['min_tempreture']}°C/{data['max_tempreture']}°C",
+                            f"Feels Like: {data['feels_like']}°C",
+                            f"Humidity: {data['humidity']}%",
+                            f"Wind: {data['wind_speed']} Km/h, {data['wind_degree']}° Degrees",
+                            f"Visibility: {data['visability'] / 1000} Km",
+                            f"Pressure: {data['pressure'] / 1000} hPa",
+                            f"Sea Level: {data['sea_level']}",
+                            f"Sunrise: {data['sunrise']} AM",
+                            f"Sunset: {data['sunset']} PM",
+                            ""
+                        )
                     else:
-                        self._update_ui(city_index, "Temperature: --°C", "Condition: --", "City not found")
+                        self._update_ui(
+                            city_index,
+                            "Temperature: --°C",
+                            "Condition: --",         
+                            "Min/Max Temp: --",
+                            "Feels Like: --",
+                            "Humidity: --",
+                            "Wind: --",
+                            "Visibility: --",
+                            "Pressure: --",
+                            "Sea Level: --",
+                            "Sunrise: --",
+                            "Sunset: --",
+                            "City not found"
+                        )
+
                 except Exception as e:
                     self._update_ui(city_index, "Temperature: --°C", "Condition: --", f"Error: {str(e)}")
         finally:
             self.is_refreshing = False
             self.view.enable_all_buttons()  # Re-enable buttons when done
 
-    def _update_ui(self, city_index, temperature_text, condition_text, error_message):
+    def _update_ui(
+        self,
+        city_index,
+        temperature_text,
+        condition_text,
+        min_max_temp,
+        feels_like,
+        humidity,
+        wind,
+        visibility,
+        pressure,
+        sea_level,
+        sunrise,
+        sunset,
+        error_message
+        ):
         """Updates the UI safely from the main thread."""
-        self.view.root.after(0, self._update_ui_safely, city_index, temperature_text, condition_text, error_message)
+        self.view.root.after(
+            0,
+            self._update_ui_safely,
+            city_index,
+            temperature_text,
+            condition_text,
+            min_max_temp,
+            feels_like,
+            humidity,
+            wind,
+            visibility,
+            pressure,
+            sea_level,
+            sunrise,
+            sunset,
+            error_message
+        )
 
-    def _update_ui_safely(self, city_index, temperature_text, condition_text, error_message):
+
+    def _update_ui_safely(
+        self,
+        city_index,
+        temperature_text,
+        condition_text,
+        min_max_temp,
+        feels_like,
+        humidity,
+        wind,
+        visibility,
+        pressure,
+        sea_level,
+        sunrise,
+        sunset,
+        error_message
+    ):
         """Updates the UI elements for a specific city."""
         self.view.update_temperature(city_index, temperature_text)
         self.view.update_condition(city_index, condition_text)
+        self.view.update_min_max_tempreture(city_index, min_max_temp)
+        self.view.update_feels_like(city_index, feels_like)
+        self.view.update_humidity(city_index, humidity)
+        self.view.update_wind(city_index, wind)
+        self.view.update_visability(city_index, visibility)
+        self.view.update_pressure(city_index, pressure)
+        self.view.update_sea_level(city_index, sea_level)
+        self.view.update_sunrise(city_index, sunrise)
+        self.view.update_sunset(city_index, sunset)
         if error_message:
             self.view.show_error(city_index, error_message)
         else:
             self.view.hide_error(city_index)
+            # Update all additional data
 
